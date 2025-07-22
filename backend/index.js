7e31c525039d99e601311533f0a2144c0d353348
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-07-21 21:42:34 -0300

    testing db connection

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,14 @@ app.get("/", (req, res) => {
     res.json({message: "Hello World"});
 });
 
+app.get("/books", (req, res)=>{
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+        if(err) return res.json(err);
+        return res.json(data);
+    });
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
