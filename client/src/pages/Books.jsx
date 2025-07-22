f9ba970f3b28237cf0a6173ff971c731a917efc3
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-07-22 09:20:03 -0300

    instaling cors and getting all books

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const Books = () => {
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async()=>{
+      try{
+        const res = await axios.get("http://localhost:3000/books");
+        // setBooks(res.data);
+        console.log(res.data);
+      }catch(error){
+        console.error("Error fetching books:", error);
+      }
+    }
+    
+    fetchBooks(); 
+  }, []); 
+
   return (
     <div>Books</div>
   )
