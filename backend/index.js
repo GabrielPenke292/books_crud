e62eefe968a3bd53737c9870738826c905c33760
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-07-21 21:38:07 -0300

    connecting to db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "books"
+});
+
+app.get("/", (req, res) => {
+    res.json({message: "Hello World"});
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
