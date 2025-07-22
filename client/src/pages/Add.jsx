d4e39ebc703170de3b4aa1e7d5dcb22806234b44
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-07-22 09:43:09 -0300

    creating logic to add new book

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export const Add = () => {
+  
+  const [book, setBook] = useState({
+    title: "",
+    description: "",
+    price: "",
+    cover: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({...prev, [e.target.name]: e.target.value}));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try{
+      await axios.post("http://localhost:3000/books", book);
+      navigate("/");
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   return (
-    <div>Add</div>
+    <div className='form'>
+      <h1>Add New Book</h1>
+      <input type="text" placeholder='Title' name='title' onChange={handleChange}/>
+      <input type="text" placeholder='Description' name='description' onChange={handleChange}/>
+      <input type="number" placeholder='Price' name='price' onChange={handleChange}/>
+      <input type="text" placeholder='Cover' name='cover' onChange={handleChange}/>
+      <button onClick={handleClick}>Add</button>
+    </div>
   )
 }   
